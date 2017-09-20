c129046d2d6dad354eb114673bef7d84abfd520a
Ojha Parmeshwar Kumar <ojha.parmeshwar-kumar@deutschebahn.com>
2017-09-20 15:14:05 +0200

    First commit

---
+++ HelloFramework/HelloFramework.h
@@ -0,0 +1,19 @@
+//
+//  HelloFramework.h
+//  HelloFramework
+//
+//  Created by Parmeshwar on 20.09.17.
+//  Copyright © 2017 DBSystel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloFramework.
+FOUNDATION_EXPORT double HelloFrameworkVersionNumber;
+
+//! Project version string for HelloFramework.
+FOUNDATION_EXPORT const unsigned char HelloFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloFramework/PublicHeader.h>
+
+
---
+++ HelloFramework/HelloLogger.swift
@@ -0,0 +1,13 @@
+//
+//  HelloLogger.swift
+//  HelloFramework
+//
+//  Created by Parmeshwar on 20.09.17.
+//  Copyright © 2017 DBSystel. All rights reserved.
+//
+
+import Foundation
+
+public func printHello() {
+    print("Hello Framweokr")
+}
